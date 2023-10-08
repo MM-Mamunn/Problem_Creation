5c463a18b5a7e75ab13456803fb49ff2bfb54569
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2023-10-08 08:16:14 +0600

    Update Solution of - Happy_game.cpp

--- Solution of - Happy_game.cpp
+++ Solution of - Happy_game.cpp
@@ -1,29 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n ,m;
+int n,m;
 int  arr[100][100];
-bool vis[100][100]={false};
+bool vis[100][100]= {false};
 
 void bfs(int i,int j)
 {
     vis[i][j] =true;
-    queue<pair<int ,int >>q;
+    queue<pair<int,int >>q;
     q.push({i,j});
     while(!q.empty())
     {
-        pair <int ,int > t = q.front();
+        pair <int,int > t = q.front();
         q.pop();
-        vis[t.first][t.second]=true;
-
         if((t.second+1)<m && vis[t.first][t.second+1] == false && arr[t.first][t.second+1] ==1 )
+        {
+            vis[t.first][t.second+1]=true;
             q.push({t.first,(t.second+1)});
+        }
         if((t.second-1)>=0 && vis[t.first][t.second-1] == false && arr[t.first][t.second-1]==1 )
+        {
+            vis[t.first][t.second-1]=true;
             q.push({t.first,(t.second-1)});
+        }
         if((t.first +1)<n && vis[t.first+1][t.second] == false && arr[t.first+1][t.second] ==1 )
-            q.push({(t.first+1),(t.second)});
+        {
+            vis[t.first+1][t.second]=true;
+            q.push({t.first+1,(t.second)});
+        }
         if((t.first -1)>=0 && vis[t.first-1][t.second] == false && arr[t.first-1][t.second] ==1 )
-            q.push({(t.first-1),(t.second)});
+        {
+            vis[t.first-1][t.second]=true;
+            q.push({t.first-1,(t.second)});
+        }
     }
 }
 
@@ -31,26 +41,26 @@ void bfs(int i,int j)
 
 int main()
 {
-int k;
-cin>>n>>m>>k;
+    int k;
+    cin>>n>>m>>k;
 
-for(int i=0;i<n;i++)
-    for(int j =0;j<m;j++)
-        cin>>arr[i][j];
-int cnt =0;
+    for(int i=0; i<n; i++)
+        for(int j =0; j<m; j++)
+            cin>>arr[i][j];
+    int cnt =0;
 
-for(int i=0;i<n;i++)
-    for(int j =0;j<m;j++)
-    {
-        if(arr[i][j] ==1 && vis[i][j] == false)
+    for(int i=0; i<n; i++)
+        for(int j =0; j<m; j++)
         {
-            cnt+=1;
-            bfs(i,j);
+            if(arr[i][j] ==1 && vis[i][j] == false)
+            {
+                cnt+=1;
+                bfs(i,j);
+            }
         }
-    }
 //cout<<"cnt "<<cnt<<endl;
-if(cnt<= k)
-    cout<<"HAPPY\n";
-else
-    cout<<"SAD\n";
+    if(cnt<= k)
+        cout<<"HAPPY\n";
+    else
+        cout<<"SAD\n";
 }
