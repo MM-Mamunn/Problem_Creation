51613f9829518e91a88fab23c1e8a1f9141fba02
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2023-12-04 23:14:32 +0600

    solution updated from bfs to dfs

--- test_case_generator_for_happy_happy_game.cpp
+++ test_case_generator_for_happy_happy_game.cpp
@@ -1,74 +1,47 @@
 #include<bits/stdc++.h>
-#include <numeric>
 using namespace std;
-#define ll long long
-#define forc(c,n) for(int i=c;i<n;i++)
-#define el "\n"
+int xm[4] = { -1, 0, 0, 1};
+int ym[4] = {0, 1, -1, 0};
+int n,m;
+int  arr[100][100];
+bool vis[100][100]= {false};
 
-int dx[]= {-1, 1, 0, 0, -1,-1, 1, 1};
-int dy[]= { 0, 0,-1, 1, -1, 1,-1, 1};
-
-
-ll arr[100][100];
-ll vis[100][100]= {0};
-ll n= 17,m=13;
-
-void dfs(ll ii,ll j)
+void dfs(int i,int j)
 {
-    vis[ii][j]=1;
-    forc(0,4)
+    vis[i][j] =true;
+    for(int l =0;l<4;l++)
     {
-        ll tx = ii + dx[i];
-        ll ty = j + dy[i];
-        if(tx>=1 && tx<=n && ty>=1 && ty<=m && vis[tx][ty] == 0 && arr[tx][ty] == 1)
-            dfs(tx,ty);
-    }
-}
-void check(ll n, ll m )
-{
-    forc(1,n+1)
-    {
-        for(int j =1 ; j<=m; j++)
-            cin>>arr[i][j];
-    }
-    memset(vis,0,sizeof(vis));
-    cout<<el<<el;
-    ll cnt =0;
-    forc(1,n+1)
-    {
-        for(int j =1 ; j<=m; j++)
+        int tx = i + xm[l];
+        int ty = j + ym[l];
+        if(tx>=0 && tx <n && ty>=0 && ty<m && vis[tx][ty]== 0 && arr[tx][ty] ==1)
         {
-            if(vis[i][j]==0 && arr[i][j] == 1)
-            {
-                cnt +=1;
-                dfs(i,j);
-            }
+            dfs(tx,ty);
         }
+
     }
-    cout<<"cnt "<<cnt<<el;
 }
 
 int main()
 {
-    ///remove comment of next two line to find the number of components that is the number of minimum pot required
-    // check(n,m);
-    //return 0;
-   /// n & m declared globally
-    forc(1,n+1)
-    {
-        for(int j =1 ; j<=m; j++)
+    int k;
+    cin>>n>>m>>k;
+
+    for(int i=0; i<n; i++)
+        for(int j =0; j<m; j++)
+            cin>>arr[i][j];
+    int cnt =0;
+
+    for(int i=0; i<n; i++)
+        for(int j =0; j<m; j++)
         {
-            /// random condition to generate 1
-            if(i % 2 && j % 3 == 2)
-                cout<<1<<" ";
-            else
-                cout<<0<<" ";
-
+            if(arr[i][j] ==1 && vis[i][j] == false)
+            {
+                cnt+=1;
+                dfs(i,j);
+            }
         }
-        cout<<el;
-    }
-    return 0;
+    if(cnt<= k)
+        cout<<"HAPPY\n";
+    else
+        cout<<"SAD\n";
 }
-
-
-
